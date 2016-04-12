81fdd59422d60b38d53ca402eb6c562682069201
Chris Chrysostom <chris.chrysostom@gmail.com>
2016-04-12 10:58:00 -0600

    Fix formatting of indentation

--- go/pinfilesrm/pinfilesrm.go
+++ go/pinfilesrm/pinfilesrm.go
@@ -2,25 +2,25 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "os/exec"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-        line := scanner.Text()
-        command := exec.Command("ipfs", "pin", "rm", line)
+		line := scanner.Text()
+		command := exec.Command("ipfs", "pin", "rm", line)
 		out, cmderr := command.Output()
-		if (out != nil) {
+		if out != nil {
 			fmt.Println(string(out))
 		}
-        if cmderr != nil {
-            log.Fatal(cmderr)
-        }
+		if cmderr != nil {
+			log.Fatal(cmderr)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
